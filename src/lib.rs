e611d44745a0f097038733cdeb641230c432793e
MaxMahem <maxtmahem@gmail.com>
2025-10-28 03:36:57 -0500

    made key error visible

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,5 @@ mod collect_failable_ext;
 mod try_from_iterator;
 
 pub use collect_failable_ext::FailableCollectExt;
-pub use try_from_iterator::TryFromIterator;
-
-#[cfg(any(feature = "hash_map", feature = "btree_map"))]
 pub use try_from_iterator::KeyCollision;
+pub use try_from_iterator::TryFromIterator;
